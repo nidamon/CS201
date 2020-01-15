73fb28b6bfe12446d3c746de558ca0e33ab6d387
nidamon <nidamon@alaska.edu>
2020-01-15 10:16:37 -0900

    Wrote some code

---
+++ HW0/HW0/HW0 p1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+	std::string input;
+	std::cout << "A Game" << std::endl;
+	std::cin >> input;
+	std::cout << "You typed" << input;
+	std::cout << std::endl;
+	return 0;
+}
