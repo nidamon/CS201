b6daff31dcc251f346a97f6cf96633395d76c035
nidamon <nidamon@alaska.edu>
2020-01-17 10:16:15 -0900

    Began to alter the code towards printing Hello World

--- HW0/HW0/HW0 p1.cpp
+++ HW0/HW0/HW0 p1.cpp
@@ -4,9 +4,7 @@
 int main(int argc, char** argv)
 {
 	std::string input;
-	std::cout << "A Game" << std::endl;
-	std::cin >> input;
-	std::cout << "You typed" << input;
+	std::cout << "Hello World" << std::endl;
 	std::cout << std::endl;
 	return 0;
 }
