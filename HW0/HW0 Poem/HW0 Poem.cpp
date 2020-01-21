e66abaf853c2dafb72478eb2e6f7853906e644de
Nathan Damon <nidamon@alaska.edu>
2020-01-20 17:33:45 -0900

    Accidentally did something to HW0 Poem that caused it to not appear in the repository, attempted to fix it.

---
+++ HW0/HW0 Poem/HW0 Poem.cpp
@@ -0,0 +1,17 @@
+//This is "Future" by Mathew Miller, it prints the entire poem.
+#include <iostream>
+
+int main()
+{
+	std::cout << "Your journey starts here it starts now" << std::endl;
+	std::cout << "Your future is in your hands and know one shows you how" << std::endl;
+	std::cout << "To make it, unfold, and have it turn out what you want it to be" << std::endl;
+	std::cout << "Not anybody, not me" << std::endl;
+	std::cout << "Talk it a step at a time" << std::endl;
+	std::cout << "Even if your plans are stopped on a dime" << std::endl;
+	std::cout << "Be careful, and listen, learn from your mistakes" << std::endl;
+	std::cout << "Because one day you'll advance on the choices you make" << std::endl;
+	std::cout << "Your future is here, your future is now" << std::endl;
+	std::cout << "But don't worry, you'll show yourself how" << std::endl;
+	return 0;
+}
