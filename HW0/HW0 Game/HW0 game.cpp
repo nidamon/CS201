b7ab455e1287587a0cabc7f216aef93dc12745ef
Nathan Damon <nidamon@alaska.edu>
2020-01-20 17:19:46 -0900

    Renamed game to HW0 game and replaced the poem, that I copied over,  with the game description

---
+++ HW0/HW0 Game/HW0 game.cpp
@@ -0,0 +1,16 @@
+//This is a program that prints a description of minecraft, adapted from wiki.
+#include <iostream>
+
+int main()
+{
+	std::cout << "Minecraft is a sandbox game created by Markus Persson, released by Mojang" << std::endl;
+	std::cout << "in 2011, and purchased by microsoft in 2014. It is the single best selling" << std::endl;
+	std::cout << "game of all time, with over 180 million copies sold by late 2019." << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "The game world is a blocky, pixelated, procedurally-generated 3D world" << std::endl;
+	std::cout << "filled with many different biomes, structures, terrain formations, and" << std::endl;
+	std::cout << "adventure. One thing that makes minecraft stand out the most is the" << std::endl;
+	std::cout << "ability to create and destroy, and the lack world boundaries. Players" << std::endl;
+	std::cout << "can travel great distances and make grand structures of their own." << std::endl;
+	return 0;
+}
