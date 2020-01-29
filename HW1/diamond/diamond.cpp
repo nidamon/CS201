6d56772c22de23b3e1202ec82e6b97350b6c767e
nidamon <nidamon@alaska.edu>
2020-01-29 10:21:30 -0900

    Began to write the code for the diamond

---
+++ HW1/diamond/diamond.cpp
@@ -0,0 +1,26 @@
+/*Nathan Damon
+CS 201
+1/29/2020
+This program will take a positive integer input and output a diamond with a size based on the integer given.
+*/
+
+#include <iostream>
+using std::cout
+using std::cin
+using std::endl
+
+int main()
+{
+	int n;
+
+	while (True) {
+
+		cout << "Please enter a positive integer";
+		cin >> n;
+		if (n > 0) {
+			break;
+		}
+		cout << 
+	}
+	
+}
