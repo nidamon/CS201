c1a7f8df608e4dd490070cddcdee56bc427a31b2
nidamon <nidamon@alaska.edu>
2020-01-29 10:23:10 -0900

    completed the program

--- HW0/HW0/HW0 p1.cpp
+++ HW0/HW0/HW0 p1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <string>
+
 
 int main(int argc, char** argv)
 {
-	std::string input;
 	std::cout << "Hello World" << std::endl;
-	std::cout << std::endl;
+
 	return 0;
 }
