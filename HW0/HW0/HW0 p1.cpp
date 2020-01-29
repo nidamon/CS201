bee11966bd7ac0f3b7ce24082d35d307dcf20345
nidamon <nidamon@alaska.edu>
2020-01-29 10:25:45 -0900

    Revert "completed the program"
    This reverts commit c1a7f8df608e4dd490070cddcdee56bc427a31b2.

--- HW0/HW0/HW0 p1.cpp
+++ HW0/HW0/HW0 p1.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include <string>
 
 int main(int argc, char** argv)
 {
+	std::string input;
 	std::cout << "Hello World" << std::endl;
-
+	std::cout << std::endl;
 	return 0;
 }
