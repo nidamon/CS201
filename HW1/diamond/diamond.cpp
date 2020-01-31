6193037c1e4dfc66e5ab038e5e99e976b16036d3
nidamon <nidamon@alaska.edu>
2020-01-31 09:34:57 -0900

    Finished the while loop and began the for loop to print the diamond.

--- HW1/diamond/diamond.cpp
+++ HW1/diamond/diamond.cpp
@@ -5,22 +5,27 @@ This program will take a positive integer input and output a diamond with a size
 */
 
 #include <iostream>
-using std::cout
-using std::cin
-using std::endl
+using std::cout;
+using std::cin;
+using std::endl;
 
-int main()
-{
+int main() {
 	int n;
 
-	while (True) {
+	while (true) {
 
-		cout << "Please enter a positive integer";
+		cout << "Please enter a positive integer ";
 		cin >> n;
-		if (n > 0) {
+		if (n >= 0) {
 			break;
 		}
-		cout << 
+		else {
+			cout << n << " needs to be a positive integer" << endl;
+		}
 	}
-	
+	cout << "You entered: " << n << endl;
+	for (int i = 1; i <= n; i++) {
+
+	}
+	return 0;
 }
