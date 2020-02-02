529719839a127fda79150f1388be62197767d146
Nathan Damon <nidamon@alaska.edu>
2020-02-02 13:24:46 -0900

    Created milekm and began to set up the code.

---
+++ HW1/mileskm/mileskm.cpp
@@ -0,0 +1,19 @@
+/*Nathan Damon
+CS 201
+2/2/2020
+This program will convert miles to kilometers.
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+	int n = 0;
+
+	cout << "Enter a number of miles for kilometer conversion: ";
+	cin >> n;
+
+	return 0;
+}
