4a6d6b74be1c2991d4077e7a9f75ea47f4f5b56f
Nathan Damon <nidamon@alaska.edu>
2020-02-02 10:18:48 -0900

    Encountered an issue with the debugger, resolved it. Fixed a lot of small errors everywhere and made sure it runs properly.

--- HW1/greatest/greatest.cpp
+++ HW1/greatest/greatest.cpp
@@ -10,22 +10,27 @@ using std::cin;
 using std::endl;
 
 int main() {
-	int n
-	int greatest = 0
+	int n = 1;
+	int greatest = 0;
 
 	cout << "Enter a sequence of positive integers, ending with zero." << endl;
-	cout << "I will prin the greatest positive number entered." << endl;
+	cout << "I will print the greatest positive number entered." << endl;
 
-	while (n != 0) { //This while loop asks for the positive integer
+	while (n > 0) { //This while loop asks for the positive integer
 
 		cout << "Enter a positive integer (0 to end): ";
 		cin >> n;
 		if (n > greatest) {
-			greatest = n
+			greatest = n;
 		}
-	
+	}
 	if (greatest == 0) {
+
 		cout << "No positive integers were entered." << endl;
+	}
+	else {
+
+		cout << "The greatest number entered: " << greatest << endl;
 	}
 	return 0;
 }
