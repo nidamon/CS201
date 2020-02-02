41f830712424e9c11c63baf588b812e06603c75d
Nathan Damon <nidamon@alaska.edu>
2020-02-02 10:35:57 -0900

    Created dotcross

---
+++ HW1/dotcross/dotcross.cpp
@@ -0,0 +1,14 @@
+/*Nathan Damon
+CS 201
+2/2/2020
+This program will calculate the dot product of a 3-component vector.
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+}
