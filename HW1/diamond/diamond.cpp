66b38085a3ff971043df3b4638694ea39752a6d6
Nathan Damon <nidamon@alaska.edu>
2020-02-02 02:05:40 -0900

    Encountered a problem with the console not staying up , so there is a system("pause") in there. Finished the for loops after a great deal effort moving things around and lots of testing.

--- HW1/diamond/diamond.cpp
+++ HW1/diamond/diamond.cpp
@@ -10,25 +10,42 @@ using std::cin;
 using std::endl;
 
 int main() {
-	int n=0;
+	int n = 0;
+	int k = 0;
 
 	while (n < 1) {
 
 		cout << "Please enter a positive integer ";
 		cin >> n;
-		if (n >= 1) {
-			break;
-		}
-		else {
+		if (n <= 0) {
 			cout << n << " needs to be a positive integer" << endl;
 		}
 	}
 	cout << "You entered: " << n << endl;
-	for (int i = 0; i <= n; i=i++) {
-		for (int i = n; i >= 0; i=i-1) {
+	for (int i = 1; i <= n; i++ , k++) {
+
+		for (int j = n - 1; j >= k; j = j - 1) { //This will put the spaces in
+
 			cout << " ";
 		}
+		for (int l = 0; l <= k * 2; l = l++) { //This will place the "#" after the spaces
+
 			cout << "#";
+		}
+		cout << endl;
 	}
+	for (int g = n - 1; g >= 1; g--) {
+
+		for (int h = n; h >= g; h--) { //This will put the spaces in
+
+			cout << " ";
+		}
+		for (int m = 1; m <= (2 * g - 1); m++) { //This will place the "#" after the spaces
+
+			cout << "#";
+		}
+		cout << endl;
+	}
+	system("pause"); //Was having issues with the console instantly closing on me. So I found this tempory solution.
 	return 0;
 }
