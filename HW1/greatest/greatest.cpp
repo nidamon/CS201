da9d2ef89090cc8673d81d050e15028a121fdedf
Nathan Damon <nidamon@alaska.edu>
2020-02-02 09:30:00 -0900

    Created and began to write the code.

---
+++ HW1/greatest/greatest.cpp
@@ -0,0 +1,14 @@
+/*Nathan Damon
+CS 201
+2/2/2020
+This program will print positive integers inputted by the user and outputs the highest integer given when zero or a negative number is inputted.
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+}
