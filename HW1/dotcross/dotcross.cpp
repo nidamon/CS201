a44edfcf55211f9851459b8f6c00b836ab3f6563
Nathan Damon <nidamon@alaska.edu>
2020-02-02 11:01:05 -0900

    Wrote out the code to ask for vectors and the code to return the dot and cross product

--- HW1/dotcross/dotcross.cpp
+++ HW1/dotcross/dotcross.cpp
@@ -1,14 +1,40 @@
 /*Nathan Damon
 CS 201
 2/2/2020
-This program will calculate the dot product of a 3-component vector.
+This program will calculate the dot product and cross product of a 3-component vector.
 */
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 using std::cout;
 using std::cin;
 using std::endl;
 
 int main() {
+	int Ax = Ay = Az = Bx = By = Bz = Cx = Cy = Cz = 0;
 
+	cout << "Enter the x, y, and z for vector A:" << endl; //asks for the components for vector A
+	cout << "x = ";
+	cin >> Ax;
+	cout << "y = ";
+	cin >> Ay;
+	cout << "z = ";
+	cin >> Az;
+
+	cout << "Enter the x, y, and z for vector B:" << endl; //asks for the components for vector B
+	cout << "x = ";
+	cin >> Bx;
+	cout << "y = ";
+	cin >> By;
+	cout << "z = ";
+	cin >> Bz;
+
+	cout << "A dot B = " << std::setprecision(5) << Ax * Bx + Ay * By + Az * Bz << endl;
+	cout << "A cross B = (";
+	cout << std::setprecision(5) << Ay * Bz - Az * By << ",";
+	cout << std::setprecision(5) << Az * Bx - Ax * Bz << ",";
+	cout << std::setprecision(5) << Ax * By - Ay * Bx << "," << endl;
+	
+	return 0;
 }
