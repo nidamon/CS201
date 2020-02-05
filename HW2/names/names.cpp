a9481dc9e90cf08137d3b8f82397c901052fc32b
nidamon <nidamon@alaska.edu>
2020-02-05 09:26:17 -0900

    Created and began to create the code for names.

---
+++ HW2/names/names.cpp
@@ -0,0 +1,18 @@
+/*Nathan Damon
+CS 201
+2/5/2020
+This program will take 10 names as input from the user and do something interesting.
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main(int argc, char** argv)
+{
+	std::vector<std::string> names
+		for (int = 0; i < 10; i++) {
+
+		}
+}
