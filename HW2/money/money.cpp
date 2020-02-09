6a74257dd22369858a111d6907252118505ddedc
Nathan Damon <nidamon@alaska.edu>
2020-02-09 10:29:46 -0900

    Added the if statements to make the output of cents be grammatically correct.

--- HW2/money/money.cpp
+++ HW2/money/money.cpp
@@ -19,17 +19,57 @@ int half_dollars;
 
 int main()
 {
-	cout << "Enter a number of pennies: ";
+	cout << "How many pennies do you have? ";
 	cin >> pennies;
-	cout << "Enter a number of nickels: ";
+	cout << "How many nickels do you have? ";
 	cin >> nickels;
-	cout << "Enter a number of dimes: ";
+	cout << "How many dimes do you have? ";
 	cin >> dimes;
-	cout << "Enter a number of quarters: ";
+	cout << "How many quarters do you have? ";
 	cin >> quarters;
-	cout << "Enter a number of half dollars: ";
+	cout << "How many half dollars do you have? ";
 	cin >> half_dollars;
+	cout << endl;
 
+	if (pennies == 1){ //gives the number of pennies
+		cout << "You have 1 penny." << endl;
+	}
+	else{
+		cout << "You have " << pennies << " pennies." << endl;
+	}
 
+	if (nickels == 1){ //gives the number of nickels
+		cout << "You have 1 nickel." << endl;
+	}
+	else{
+		cout << "You have " << nickels << " nickels." << endl;
+	}
+
+	if (dimes == 1){ //gives the number of dimes
+		cout << "You have 1 dime." << endl;
+	}
+	else{
+		cout << "You have " << dimes << " dimes." << endl;
+	}
+
+	if (quarters == 1){ //gives the number of quarters
+		cout << "You have 1 quarter." << endl;
+	}
+	else{
+		cout << "You have " << quarters << " quarters." << endl;
+	}
+
+	if (half_dollars == 1){ //gives the number of half dollars
+		cout << "You have 1 half dollar." << endl;
+	}
+	else{
+		cout << "You have " << half_dollars << " half dollars." << endl;
+	}
+	cout << endl;
+	
+
+	int q;
+	cout << "Program has finished. Enter a character to return: ";
+	cin >> q; //Stops the console from closing after the program has run.
 	return 0;
 }
