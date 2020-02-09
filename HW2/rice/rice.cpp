3d81abaa7416222ce08e36088473035216955ab7
Nathan Damon <nidamon@alaska.edu>
2020-02-09 14:49:18 -0900

    Un-commented the if statement and changed the doubles back to ints as well as changed the answers to the questions.

--- HW2/rice/rice.cpp
+++ HW2/rice/rice.cpp
@@ -2,7 +2,7 @@
 Nathan Damon
 CS 201
 2/9/2020
-This program will add up the grains of rice that double with each square of the chess board.
+This program will add up the grains of rice that double with each square of the chess board till they reach or pass the number inputted.
 */
 
 #include <iostream>
@@ -12,30 +12,30 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-double rice_on_square = 1;
-double total_rice = 0;
+int rice_on_square = 1;
+int total_rice = 0;
 int num_rice;
 
 int main()
 {
 	cout << "Enter a number of rice and see how many squares it takes to get that much: ";
-	cin >> num_rice;
+	cin >> num_rice; //Allows the user to determine the number of rice to count up to.
 
 	for (int square = 0; square < 64; square++)
 	{
-		rice_on_square = pow(2, square);
-		total_rice = total_rice + rice_on_square;
-			cout << rice_on_square << "       total " << total_rice << " " << square <<  endl;
-		/*if (total_rice > num_rice)
+		rice_on_square = pow(2, square); //Doubles the rice for the next square
+		total_rice = total_rice + rice_on_square; //Adds up the total rice
+		if (total_rice > num_rice)
 		{
 			cout << "It takes " << square << " squares to reach " << num_rice << " grains of rice." << endl;
 			break;
 		}
-		*/
+		
 	}
 	cout << endl;
-	cout << "The largest number of squares that can be used to accurrately calculate the number of grains (using an int) is 31." << endl;
-	cout << "The largest number of squares that can be used to accurrately calculate the number of grains (using an int) is 64." << endl;
+	cout << "The largest number of squares that can be used to accurrately calculate the number of grains:" << endl;
+	cout << "(using an int) is 31 squares." << endl;
+	cout << "(using a double) is 64 squares." << endl;
 	cout << endl;
 
 	int q;
