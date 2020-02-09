4bba71551db92d277f76903384565bba14c7d7da
Nathan Damon <nidamon@alaska.edu>
2020-02-09 00:43:31 -0900

    Set up InputNames function and added an input at the end of main function to stop the console from closing.

--- HW2/names/names.cpp
+++ HW2/names/names.cpp
@@ -1,4 +1,5 @@
-/*Nathan Damon
+/*names
+Nathan Damon
 CS 201
 2/5/2020
 This program will take 10 names as input from the user and do something interesting.
@@ -9,10 +10,26 @@ This program will take 10 names as input from the user and do something interest
 #include <string>
 #include <algorithm>
 
+using std::cin;
+
+void InputNames(std::vector<std::string> & names);
+
 int main(int argc, char** argv)
 {
-	std::vector<std::string> names
-		for (int = 0; i < 10; i++) {
+	std::vector<std::string> names;
+	InputNames(names);
 
-		}
+	int q;
+	cin >> q;
+	return 0;
+}
+
+void InputNames(std::vector<std::string> & names)
+{
+	for (int i = 0; i < 10; i++) {
+		std::string name;
+		std::cout << "Please enter a name: ";
+		std::getline(cin, name);
+		names.push_back(name);
+	}
 }
