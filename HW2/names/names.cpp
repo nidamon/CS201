8bbd6922b074183e15d11b6b92589501a4678dec
Nathan Damon <nidamon@alaska.edu>
2020-02-09 01:27:16 -0900

    Created PrintNames to help with confirming whether or not InputNames works. Made additional inprovements here and there.

--- HW2/names/names.cpp
+++ HW2/names/names.cpp
@@ -11,25 +11,45 @@ This program will take 10 names as input from the user and do something interest
 #include <algorithm>
 
 using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
 
-void InputNames(std::vector<std::string> & names);
+void InputNames(vector<string>& names);
+bool DoesNameExist(const string& anmeToFind, const vector<string>& names);
+void PrintNames(const vector<string>& names);
 
 int main(int argc, char** argv)
 {
-	std::vector<std::string> names;
+	vector<string> names;
 	InputNames(names);
+	PrintNames(names);
 
-	int q;
-	cin >> q;
+	int q; 
+	cout << "Program has finished. Enter a character to return: ";
+	cin >> q; //Stops the console from closing after the program has run.
 	return 0;
 }
 
-void InputNames(std::vector<std::string> & names)
+void InputNames(vector<string>& names) //Adds the inputted names to the vector "names"
 {
-	for (int i = 0; i < 10; i++) {
-		std::string name;
-		std::cout << "Please enter a name: ";
+	for (int i = 0; i < 10; i++) 
+	{
+		string name;
+		cout << "Please enter a name: ";
 		std::getline(cin, name);
 		names.push_back(name);
 	}
+	cout << endl;
+}
+
+void PrintNames(const vector<string>& names) //Prints the contents of the vector "names"
+{
+	cout << "Below is the list of names." << endl;
+	for (int i = 0; i < names.size(); i++)
+	{
+		cout << names[i] << " " << endl;
+	}
+	cout << endl;
 }
