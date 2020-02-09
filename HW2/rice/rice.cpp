5e6bc64604a19da585c6b19d15a58b1baec4ba79
Nathan Damon <nidamon@alaska.edu>
2020-02-09 14:25:26 -0900

    Created and added the code for the for loop and put in a prompt to ask for the number of grains of rice to add up to.

---
+++ HW2/rice/rice.cpp
@@ -0,0 +1,41 @@
+/*rice
+Nathan Damon
+CS 201
+2/9/2020
+This program will add up the grains of rice that double with each square of the chess board.
+*/
+
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int rice_on_square = 1;
+int total_rice = 0;
+int num_rice;
+
+int main()
+{
+	cout << "Enter a number of rice and see how many squares it takes to get that much: ";
+	cin >> num_rice;
+
+	for (int square = 0; square < 64; square++)
+	{
+		rice_on_square = pow(2, square);
+		total_rice = total_rice + rice_on_square;
+			cout << rice_on_square << "       total " << total_rice << endl;
+		if (total_rice > num_rice)
+		{
+			cout << "It takes " << square << " squares to reach " << num_rice << " grains of rice." << endl;
+			break;
+		}
+		
+	}
+	
+	int q;
+	cout << "Program has finished. Enter a character to return: ";
+	cin >> q; //Stops the console from closing after the program has run.
+	return 0;
+}
