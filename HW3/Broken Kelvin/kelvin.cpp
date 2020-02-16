c19f88b53bd462f6b64ac6586089d80f85e312f6
Nathan Damon <nidamon@alaska.edu>
2020-02-16 13:08:31 -0900

    Created Broken Kelvin and began to correct it.

---
+++ HW3/Broken Kelvin/kelvin.cpp
@@ -0,0 +1,26 @@
+/*kelvin.cpp
+Nathan Damon
+CS 201
+2/16/2020
+This program will take in a tempurature in Celsius and will convert it to Kelvin.
+*/
+
+// Converts Celsius to Kelvin
+
+#include <iostream> // Did not have iostream or the using std::... below.
+using std::cin;
+using std::cout;
+
+double ctok(double c)
+{
+	double k = c + 273.15 // If k is an int then the 0.15 that is added to c will be lost.
+		return int;
+}
+
+int main()
+{
+	double c = 0;
+	cin >> d;
+	double k = ctok("c");
+	Cout << k << '/n';
+}
