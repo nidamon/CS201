b0b2362999d6aa41da019b350d09c0656ab20516
Nathan Damon <nidamon@alaska.edu>
2020-02-16 13:40:06 -0900

    Made many changes and added a number of comments and improvements.

--- HW3/Broken Kelvin/kelvin.cpp
+++ HW3/Broken Kelvin/kelvin.cpp
@@ -2,7 +2,8 @@
 Nathan Damon
 CS 201
 2/16/2020
-This program will take in a tempurature in Celsius and will convert it to Kelvin.
+This program will take in a tempurature in Celsius and will convert it 
+	to Kelvin and specify in comments what was changed to fix it.
 */
 
 // Converts Celsius to Kelvin
@@ -10,17 +11,31 @@ This program will take in a tempurature in Celsius and will convert it to Kelvin
 #include <iostream> // Did not have iostream or the using std::... below.
 using std::cin;
 using std::cout;
+using std::endl;
 
 double ctok(double c)
 {
-	double k = c + 273.15 // If k is an int then the 0.15 that is added to c will be lost.
-		return int;
+	while (c < -273.15)
+	{
+		cout << "Enter a value greater than -273.15: ";
+		cin >> c;
+	}
+		
+	double k = c + 273.15; // If k is an int then the 0.15 that is added to c will be lost.
+		return k; // We are returning k not a type.
 }
 
 int main()
 {
 	double c = 0;
-	cin >> d;
-	double k = ctok("c");
-	Cout << k << '/n';
+	cout << "Enter a tempurature in Celsius and it shall be converted to Kelvin: "; // The user should recieve a prompt about what to input.
+	cin >> c; // We are asking for c not some new undeclared variable d.
+
+	if (c < -273.15) { // We need to check for when the inputted value of c is less than -273.15 as the tempurature cannot be below that.
+		cout << "A tempurature less than -273.15 degrees Celsius cannot be evaluated." << endl;
+	}
+	double k = ctok(c); // c should not be in quotes.
+	cout << k - 273.15 << " degrees Celsius is equal to "; // This will make things easier to read/understand.
+	cout << k << " degrees Kelvin" << endl; /* /n should not be in single quotes and it's better to use endl here instead of /n.
+						Cout should be lower case cout. The units of k should be outputted with k.*/
 }
