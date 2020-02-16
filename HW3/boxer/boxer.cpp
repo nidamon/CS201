aec2c1966860911dab6b6b014e84f68a2b107b17
Nathan Damon <nidamon@alaska.edu>
2020-02-16 00:00:14 -0900

    Added in many comments to provide explainations to many parts of the code in boxer.cpp and boxprint.cpp.

--- HW3/boxer/boxer.cpp
+++ HW3/boxer/boxer.cpp
@@ -9,44 +9,45 @@ This program will take a string and integer input and output a box.
 #include "boxer.h"
 #include <vector>
 
-using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
 
 void boxer(
-	string str,
-	int n
+	string str, //This string will be put into the middle of the box.
+	int n //This integer determine the thickness of the box.
 )
 {
-	cout << endl;
+	cout << endl; /*This is an additional blank line to seperate the 
+					outputted box from the prompt text of the main file*/
+
 	for (int i = 0; i < n; i++){ //This for loop will output the top block of "*".
 		for (int i = 0; i < 2 + 2 * n + str.size(); i++) 
 			cout << "*";
 		cout << endl;
 	}
 
-	vector <string> hold(n, "*");//This vector will hold some characters to lessen the number of for loops.
-	hold.resize(2 + n + str.size(), " ");
-	hold.resize(2 + 2 * n + str.size(), "*");
+	vector <string> hold(n, "*");//This vector will contain some characters to lessen the number of for loops.
+	hold.resize(2 + n + str.size(), " "); // Adds the spaces for the blank space in the box.
+	hold.resize(2 + 2 * n + str.size(), "*"); // Adds the remaning "*" to the end.
 
-	for (int i = 0; i < hold.size(); i++)
+	for (int i = 0; i < hold.size(); i++) // Prints the elements in the vector "hold".
 		cout << hold[i];
 
 	cout << endl;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) // Prints the "*" before the string.
 		cout << "*";
 
-	cout << " " << str << " ";
+	cout << " " << str << " "; // The string with spacing.
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) // Adds the last "*" after the string.
 		cout << "*";
 
 	cout << endl;
 
-	for (int i = 0; i < hold.size(); i++)
+	for (int i = 0; i < hold.size(); i++)// Prints the elements in the vector "hold" again.
 		cout << hold[i];
 
 	cout << endl;
@@ -56,5 +57,5 @@ void boxer(
 			cout << "*";
 		cout << endl;
 	}
-	cout << endl;
+	cout << endl; //This is also an additional blank line for seperation in the output on the console.
 }
--- HW3/boxer/boxprint.cpp
+++ HW3/boxer/boxprint.cpp
@@ -15,21 +15,21 @@ using std::string;
 
 int main()
 {
-	string str;
-	int n;
-	int k=0;
+	string str; // String displayed in the box.
+	int n; // Determines the box border thickness.
+	int k=0; // Is for the while loop (Do more boxes?).
 
 	cout << "This program will print a box with a word or phrase in it." << endl;
 	while (k == 0){
-		cout << "Please specify a word or phrase that you want in the box: ";
+		cout << "Please specify a word or phrase that you want in the box. You cannot put any spaces in it: ";
 		cin >> str;
 		cout << "Please specify how thick you want the border of the box: ";
 		cin >> n;
-		while (n < 1) {
+		while (n < 1) { // If n is not 1 or more, user is prompted again.
 			cout << "The border's thickness cannot be less then 1. Enter a number greater than 0: ";
 			cin >> n;
 		}
-		boxer(str,n);
+		boxer(str,n); // Function that puts the box together.
 
 		cout << endl;
 		cout << "If you want to make and print another custom box type 0, If you do not, type a different number: ";
