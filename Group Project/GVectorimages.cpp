5db930408b68291134faebb4675dcd26553a0054
Nathan Damon <nidamon@alaska.edu>
2020-02-19 23:26:08 -0900

    Began creation and test printing of the vectors with strings.

--- Group Project/GVectorimages.cpp
+++ Group Project/GVectorimages.cpp
@@ -11,3 +11,33 @@ This program will contain a set of vectors that hold strings for a game.
 using std::vector;
 using std::string;
 
+vector <vector <string>> vblank;
+vector <string> vblank1;
+
+string empty = "|             |";
+
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void main()
+{
+	for (int i = 0; i < 6; i++) {
+		vblank1.push_back(empty);
+	}
+
+	cout << "push done" << endl;
+
+	for (int i = 0; i < 6; i++) {
+		cout << vblank1[i] << endl;
+	}
+
+	cout << empty.size() << endl;
+
+	int nooooo;
+	cout << endl;
+	cout << "To stop, type something: ";
+	cin >> nooooo;
+}
