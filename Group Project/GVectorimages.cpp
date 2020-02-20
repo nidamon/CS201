08397e266338d2e99005d060c216ccf74b035b8c
Nathan Damon <nidamon@alaska.edu>
2020-02-19 22:34:45 -0900

    Created GMain.cpp and GVectorimages.cpp. Left GMain blank but began to set up GVectorimages.

---
+++ Group Project/GVectorimages.cpp
@@ -0,0 +1,13 @@
+/*GVectorimages.cpp
+Nathan Damon
+CS 201
+2/19/2020
+This program will contain a set of vectors that hold strings for a game.
+*/
+
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::string;
+
