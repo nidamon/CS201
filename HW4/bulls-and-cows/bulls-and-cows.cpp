fc199f9bba512bc67c56a2fbc6d6db8c3c4d31ff
Nathan Damon <nidamon@alaska.edu>
2020-02-23 12:12:34 -0900

    Created bulls-and-cows.cpp with its description.

---
+++ HW4/bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,9 @@
+/*bulls-and-cows.cpp
+Nathan Damon
+CS 201
+2/23/2020
+This program will create a vector of four integers and ask the user to guess them.
+*/
+
+#include <iostream>
+#include <vector>
