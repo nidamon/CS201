2854e158bb051147329b0cddf1d367a3cae4df30
Nathan Damon <nidamon@alaska.edu>
2020-02-23 18:53:49 -0900

    Created fifo-lifo.cpp and made the comment for it as well put the #included and std::using in.

---
+++ HW4/fifo-lifo/fifo-lifo.cpp
@@ -0,0 +1,20 @@
+/*fifo-lifo.cpp
+Nathan Damon
+CS 201
+2/23/2020
+This program will allow the user to put items into a conainer and take them out as well as display them.
+*/
+
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	return 0;
+}
+
