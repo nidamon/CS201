80fe8d8c17dd3b432177809da608b2c40c0fd3be
Nathan Damon <nidamon@alaska.edu>
2020-03-01 22:26:48 -0900

    Made major changes to how the player and barrier cells were created, increased the size of the cells, and created a sense of falling when testing. GVectorimages.cpp.

--- Group Project/GVectorimages.cpp
+++ Group Project/GVectorimages.cpp
@@ -10,6 +10,8 @@ This program will contain a set of vectors that hold strings for a game.
 #include <cstdlib>
 #include <ctime>
 
+#include <Windows.h>
+
 using std::vector;
 using std::string;
 
@@ -18,17 +20,57 @@ vector <string> empty_cell;
 vector <string> barrier_cell;
 vector <string> player_cell;
 
-string empty =            "|             |";
-string outside_barrier =  "|-------------|";
-string inside_barrier =   "|#############|";
+string empty =            "|                            |";
+string outside_barrier =  "|----------------------------|";
+string inside_barrier =   "|############################|";
 
-string player_model_1_0 = "|   !     !   |";
-string player_model_1_1 = "| ! ! .-. ! ! |";  // Temporary gingerbread man
-string player_model_1_2 = "| ! _( \" )_ ! |";
-string player_model_1_3 = "|  (_  :  _)  |";
-string player_model_1_4 = "|    / ' \\    |";
-string player_model_1_5 = "|   (_/^\\_)   |";
-string player_model_1_6 = "|             |";
+
+vector <string> barrier_model_1 = { // Rabbit box
+ "|                            |",
+ "|   H>==================<H   |",
+ "|   Y \\                / Y   |",
+ "|   |  \\ ____________ /  |   |",
+ "|   |   |            |   |   |",
+ "|   |   |            |   |   |",
+ "|   |   |            |   |   |",
+ "|   |   |(\\___/)     |   |   |",
+ "|   |   |(=\'.\'=)_____|   |   |",
+ "|   | _- (\")_(\")      -_ |   |",
+ "|   I>==================<I   |",
+ "|                            |",
+};
+
+// Player Models below.
+
+vector <string> player_model_1 = { // Temporary gingerbread man
+ "|                            |",
+ "|                            |",  
+ "|                            |",
+ "|       AAAAHHHHH!!!!!       |",
+ "|                            |",
+ "|           !     !          |",
+ "|         ! ! .-. ! !        |",
+ "|         ! _( \" )_ !        |",
+ "|          (_  :  _)         |",
+ "|            / ' \\           |",
+ "|           (_/^\\_)          |",
+ "|                            |",
+};
+
+vector <string> player_model_2 = { // The Bat
+ "|                            |",
+ "|                            |",  
+ "|                            |",
+ "|                            |",
+ "|     (,_    ,_,    _,)      |",
+ "|     /|\\`-._( )_.-'/|\\      |",
+ "|    / | \\`'-/ \\-'`/ | \\     |",
+ "|   /  |_.'-.\\ /.-'._|  \\    |",
+ "|  /_.-'      \"      `-._\\   |",
+ "|                            |",
+ "|                            |",
+ "|                            |",
+};
 
 using std::cout;
 using std::cin;
@@ -45,28 +87,20 @@ int main()
 	int barrier_count = 0;
 	int randvalue = 0;
 
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < 12; i++) {
 		empty_cell.push_back(empty);
 	}
 
-    player_cell.push_back(player_model_1_0);
-	player_cell.push_back(player_model_1_1);
-	player_cell.push_back(player_model_1_2);
-	player_cell.push_back(player_model_1_3);
-	player_cell.push_back(player_model_1_4);
-	player_cell.push_back(player_model_1_5);
-	
+	player_cell = player_model_2;
 
-	barrier_cell.push_back(outside_barrier);
-	barrier_cell.push_back(inside_barrier);
-	barrier_cell.push_back(outside_barrier);
-	barrier_cell.resize(barrier_cell.size() + 3, empty);
+	barrier_cell = barrier_model_1;
 
-	for (int i = 0; i < 6; i++) {  // For testing the output of the cells.
+
+	for (int i = 0; i < 12; i++) {  // For testing the output of the cells.
 		cout << empty_cell[i] << endl;
 	}
 
-	barrier_section.push_back(empty_cell);
+	barrier_section.push_back(empty_cell); // Needs refinement.
 	barrier_section.push_back(barrier_cell);
 
 	int yes = 1;
@@ -95,18 +129,37 @@ int main()
 			Vrandom = { 0 , 0 , 1 };
 			break;
 		}
-	// repeats the previous bottom layer.
-		for (int i = 0; i < 6; i++) {
-			cout << barrier_section[Vlast_generated[0]][i] << barrier_section[Vlast_generated[1]][i] << barrier_section[Vlast_generated[2]][i] << endl;
+
+		int image_movement = 0;
+		while (image_movement != 6)
+		{
+			system("cls");
+
+			// Generates the middle layer with the player model.
+			for (int i = image_movement; i < 12; i++) {
+				cout << empty_cell[i] << empty_cell[i] << empty_cell[i] << endl;
+			}
+			// repeats the previous bottom layer.
+			for (int i = 0; i < 12; i++) {
+				cout << barrier_section[Vlast_generated[0]][i] << barrier_section[Vlast_generated[1]][i] << barrier_section[Vlast_generated[2]][i] << endl;
+			}
+			// Generates the middle layer with the player model.
+			for (int i = 0; i < 12; i++) {
+				cout << empty_cell[i] << player_cell[i] << empty_cell[i] << endl;
+			}
+			// Generates the bottom layer.
+			for (int i = 0; i < 12; i++) {
+				cout << barrier_section[Vrandom[0]][i] << barrier_section[Vrandom[1]][i] << barrier_section[Vrandom[2]][i] << endl;
+			}
+
+			// Generates the middle layer with the player model.
+			for (int i = 0; i < image_movement; i++) {
+				cout << empty_cell[i] << empty_cell[i] << empty_cell[i] << endl;
+			}
+
+			image_movement = image_movement + 2;
 		}
-	// Generates the middle layer with the player model.
-		for (int i = 0; i < 6; i++) {
-			cout << empty_cell[i] << player_cell[i] << empty_cell[i] << endl;
-		}
-	// Generates the bottom layer.
-		for (int i = 0; i < 6; i++) {
-			cout << barrier_section[Vrandom[0]][i] << barrier_section[Vrandom[1]][i] << barrier_section[Vrandom[2]][i] << endl;
-		}
+
 	// Sets Vlast_generated to the current Vrandom's values.
 		for (int i = 0; i < 3; i++)
 			Vlast_generated[i] = Vrandom[i];
@@ -121,6 +174,47 @@ int main()
 		system("cls");
 	}
 	
+
+
+	// Small test movement test below
+
+	vector<string> S_a = { "_" , "|" , "_" , "_" , "|" , "_" , "|" , "_" };
+	vector<string> S_b = { "_" , "_" , "_" , "_" , "|" , "_" , "_" , "_" };
+	vector<string> S_c = { "_" , "_" , "|" , "_" , "_" , "|" , "_" , "|" };
+
+	yes = 1;
+	repititions = 0;
+	while (yes == 1) {
+
+		// repeats the previous bottom layer.
+		for (int i = (repititions); i < (repititions + 4); i++) {
+			cout << S_a[i % 7] << " ";
+		}
+		cout << endl;
+		for (int i = (repititions); i < (repititions + 4); i++) {
+			cout << S_b[i % 7] << " ";
+		}
+		cout << endl;
+		for (int i = (repititions); i < (repititions + 4); i++) {
+			cout << S_c[i % 7] << " ";
+		}
+
+		cout << endl;
+
+		Sleep(0700);
+
+		repititions++;
+
+		/*yes = 0;
+		cout << endl;
+		cout << "If want more samples, type 1: ";
+		cin >> yes;*/
+
+		system("cls");
+	}
+
+
+
 
 	cout << endl;
 	cout << "To stop, type something: ";
