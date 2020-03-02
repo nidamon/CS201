140a01d8ceff1cfc0c39db6a07efcb031ccd6d5c
nidamon <nidamon@alaska.edu>
2020-03-02 10:06:16 -0900

    Added the code to randomize which barrier model is displayed as well as put in many comments. GVectorimages.cpp.

--- Group Project/GVectorimages.cpp
+++ Group Project/GVectorimages.cpp
@@ -15,14 +15,13 @@ This program will contain a set of vectors that hold strings for a game.
 using std::vector;
 using std::string;
 
-vector <vector <string>> barrier_section;
+vector <vector <string>> barrier_section; // Need to change
 vector <string> empty_cell;
-vector <string> barrier_cell;
-vector <string> player_cell;
+vector <string> player_cell; // Will hold a specified player model.
 
 string empty =            "|                            |";
-string outside_barrier =  "|----------------------------|";
-string inside_barrier =   "|############################|";
+string outside_barrier =  "|----------------------------|"; // Not using at the moment.
+string inside_barrier =   "|############################|"; // Similarly not being used.
 
 
 vector <string> barrier_model_1 = { // Rabbit box
@@ -33,6 +32,36 @@ vector <string> barrier_model_1 = { // Rabbit box
  "|   |   |            |   |   |",
  "|   |   |            |   |   |",
  "|   |   |            |   |   |",
+ "|   |   |(\\___/)     |   |   |",
+ "|   |   |(=\'.\'=)_____|   |   |",
+ "|   | _- (\")_(\")      -_ |   |",
+ "|   I>==================<I   |",
+ "|                            |",
+};
+
+vector <string> barrier_model_2 = { // Rabbit box 2
+ "|                            |",
+ "|   H>==================<H   |",
+ "|   Y \\                / Y   |",
+ "|   |  \\ ____________ /  |   |",
+ "|   |   |            |   |   |",
+ "|   |   |            |   |   |",
+ "|   |   |     2      |   |   |",
+ "|   |   |(\\___/)     |   |   |",
+ "|   |   |(=\'.\'=)_____|   |   |",
+ "|   | _- (\")_(\")      -_ |   |",
+ "|   I>==================<I   |",
+ "|                            |",
+};
+
+vector <string> barrier_model_3 = { // Rabbit box 3
+ "|                            |",
+ "|   H>==================<H   |",
+ "|   Y \\                / Y   |",
+ "|   |  \\ ____________ /  |   |",
+ "|   |   |            |   |   |",
+ "|   |   |            |   |   |",
+ "|   |   |       3    |   |   |",
  "|   |   |(\\___/)     |   |   |",
  "|   |   |(=\'.\'=)_____|   |   |",
  "|   | _- (\")_(\")      -_ |   |",
@@ -76,39 +105,40 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-vector <int> Vrandom(3, 3);
-vector <int> Vlast_generated(3, 0);
+vector <int> Vrandom(3, 0); // Used for barrier randomization.
+vector <int> Vlast_generated(3, 0); // Copies down the barrier model for printing the previous set of barriers.
 
 int main()
 {
-	int stop;
+	int stop; // Is used at the end of the program to prevent the console from closing.
 	int max_barrier_count = 2;
 	int min_barrier_count = 1;
 	int barrier_count = 0;
 	int randvalue = 0;
 
-	for (int i = 0; i < 12; i++) {
+	for (int i = 0; i < 12; i++) { // Creates the empty cell
 		empty_cell.push_back(empty);
 	}
 
-	player_cell = player_model_2;
-
-	barrier_cell = barrier_model_1;
-
+	player_cell = player_model_1; 
 
 	for (int i = 0; i < 12; i++) {  // For testing the output of the cells.
 		cout << empty_cell[i] << endl;
 	}
 
 	barrier_section.push_back(empty_cell); // Needs refinement.
-	barrier_section.push_back(barrier_cell);
+	barrier_section.push_back(barrier_model_1);
+	barrier_section.push_back(barrier_model_2);
+	barrier_section.push_back(barrier_model_3);
+
+	// Main while loop for displaying the models
 
 	int yes = 1;
 	int repititions = 0;
 	while (yes == 1){
 		srand(time(0));
-		randvalue = (((rand()) + repititions) % 5);
-		switch (randvalue)
+		randvalue = (((rand()) + repititions) % 5); // Gets a random value between 0 and 5.
+		switch (randvalue) // Picks between 6 different variations of barrier setups.
 		{
 		case 0:
 			Vrandom = { 1 , 0 , 0 };
@@ -130,48 +160,76 @@ int main()
 			break;
 		}
 
+		int random_barrier_1 = 0;
+		int random_barrier_2 = 0;
+		int random_barrier_3 = 0;
+		int types_of_barriers = 3; // There are three rabbit boxes at the moment.
 		int image_movement = 0;
 		while (image_movement != 6)
 		{
-			system("cls");
+			system("cls"); // refreshes the console screen.
 
-			// Generates the middle layer with the player model.
+		// Generates the top layer the rapidly shrinks.
 			for (int i = image_movement; i < 12; i++) {
 				cout << empty_cell[i] << empty_cell[i] << empty_cell[i] << endl;
 			}
-			// repeats the previous bottom layer.
+		// repeats the previous bottom layer.
 			for (int i = 0; i < 12; i++) {
 				cout << barrier_section[Vlast_generated[0]][i] << barrier_section[Vlast_generated[1]][i] << barrier_section[Vlast_generated[2]][i] << endl;
 			}
-			// Generates the middle layer with the player model.
+		// Generates the middle layer with the player model.
 			for (int i = 0; i < 12; i++) {
 				cout << empty_cell[i] << player_cell[i] << empty_cell[i] << endl;
 			}
-			// Generates the bottom layer.
+			
+
+			if (Vrandom[0] == 1) // Picks a random barrier model.
+				random_barrier_1 = ((((rand()) + repititions) % 5) % 4); // Will need changing when more than 3 barrier models are made.
+			else
+				random_barrier_1 = 0;
+
+
+			if (Vrandom[1] == 1) // Picks a random barrier model.
+				random_barrier_2 = ((((rand()) + repititions) % 7) % 4); // Will need changing when more than 3 barrier models are made.
+			else
+				random_barrier_2 = 0;
+
+
+			if (Vrandom[2] == 1) // Picks a random barrier model.
+				random_barrier_3 = ((((rand()) + repititions) % 9) % 4); // Will need changing when more than 3 barrier models are made.
+			else
+				random_barrier_3 = 0;
+
+
+		// Generates the bottom layer.
 			for (int i = 0; i < 12; i++) {
-				cout << barrier_section[Vrandom[0]][i] << barrier_section[Vrandom[1]][i] << barrier_section[Vrandom[2]][i] << endl;
+				cout << barrier_section[random_barrier_1][i];
+				cout << barrier_section[random_barrier_2][i];
+				cout << barrier_section[random_barrier_3][i] << endl;
+
 			}
 
-			// Generates the middle layer with the player model.
+		// Generates a new layer with empty cells that increases in size.
 			for (int i = 0; i < image_movement; i++) {
 				cout << empty_cell[i] << empty_cell[i] << empty_cell[i] << endl;
 			}
 
-			image_movement = image_movement + 2;
+			image_movement = image_movement + 2; // Determines how many times the overall image makes small shifts.
 		}
 
 	// Sets Vlast_generated to the current Vrandom's values.
-		for (int i = 0; i < 3; i++)
-			Vlast_generated[i] = Vrandom[i];
+			Vlast_generated[0] = random_barrier_1;
+			Vlast_generated[1] = random_barrier_2;
+			Vlast_generated[2] = random_barrier_3;
 		
 		repititions++;
-
+	// Prompts for a continuation of repetitions
 		yes = 0;
 		cout << endl;
 		cout << "If want more samples, type 1: ";
 		cin >> yes;
 
-		system("cls");
+		system("cls"); // refreshes the console screen.
 	}
 	
 
