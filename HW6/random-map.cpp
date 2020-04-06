cce0d17d28016ebf9a7b82a7af86cc1656e7263c
Nathan Damon <nidamon@alaska.edu>
2020-04-06 00:24:33 -0800

    Added the function RandomBetweenN to random-map.cpp and made a number of changes as well as added code for testing the current two functions.

--- HW6/random-map.cpp
+++ HW6/random-map.cpp
@@ -2,29 +2,38 @@
 Nathan Damon
 CS 201
 4/04/2020
-This program will 
+This program will generate random numbers and their histograms
 */
 
 #include <iostream>
 using std::cin;
 using std::cout;
+using std::endl;
 #include <iomanip>
 #include <string>
 #include <map>
 #include <random>
 using std::random_device;
+using std::mt19937;
 #include <cmath>
 
 
-int RandomBetweenU
-(int first,
- int last,
- random_device& r);
+int RandomBetweenU(
+    int first,
+    int last,
+    random_device& r);
+
+int RandomBetweenN(
+    const int& mean,
+    mt19937& e2);
 
 int main()
 {
     // Seed with a real random value, if available
     random_device r;
+
+    std::seed_seq seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
+    mt19937 e2(seed2);
 
     int first;
     cout << "First: ";
@@ -34,34 +43,56 @@ int main()
     cout << "Last: ";
     cin >> last;
 
-    int mean = RandomBetweenU(first, last, r);
+    int mean = first + ((last - first) / 2);
 
+    cout << "U: " << RandomBetweenU(first, last, r); 
+    cout << endl;
+    cout << "N: " << RandomBetweenN(mean, e2);
+    cout << endl;
 
-    // Generate a normal distribution around that mean
-    std::seed_seq seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
-    std::mt19937 e2(seed2);
-    std::normal_distribution<> normal_dist(mean, 2);
-
-    std::map<int, int> hist;
+    std::map<int, int> hist1;
     for (int n = 0; n < 10000; ++n) {
-        ++hist[std::round(normal_dist(e2))];
+        ++hist1[std::round(RandomBetweenU(first, last, r))];
     }
-    cout << "Normal distribution around " << mean << ":\n";
-    for (auto p : hist) {
+    cout << "Uniform distribution between " << first << " and " << last << ":\n";
+    for (auto p : hist1) {
         cout << std::fixed << std::setprecision(1) << std::setw(2)
             << p.first << ' ' << std::string(p.second / 200, '*') << '\n';
     }
+
+    std::map<int, int> hist2;
+    for (int n = 0; n < 10000; ++n) {
+        ++hist2[std::round(RandomBetweenN(mean, e2))];
+    }
+    cout << "Normal distribution around " << mean << ":\n";
+    for (auto p : hist2) {
+        cout << std::fixed << std::setprecision(1) << std::setw(2)
+            << p.first << ' ' << std::string(p.second / 200, '*') << '\n';
+    }
+
+    cin >> last;
 }
 
-// Chooses a random mean between first and last
-int RandomBetweenU
-(int first, 
- int last, 
- random_device& r)
+
+
+// Returns a uniform random number between first and last
+int RandomBetweenU(
+    int first, 
+    int last, 
+    random_device& r)
 {
     std::default_random_engine e1(r());
     std::uniform_int_distribution<int> uniform_dist(first, last);
-    int mean = uniform_dist(e1);
-    std::cout << "Randomly-chosen mean: " << mean << '\n';
-    return mean;
+    return uniform_dist(e1);
+}
+
+
+
+// Returns a normally distributed random number between first and last
+int RandomBetweenN(
+    const int& mean,
+    mt19937& e2)
+{ 
+    std::normal_distribution<> normal_dist(mean, 2);
+    return normal_dist(e2);
 }
