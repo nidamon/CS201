61afcdc1406a3e741dfd1eb06613eb250ad0deae
Nathan Damon <nidamon@alaska.edu>
2020-04-08 19:31:19 -0800

    Implemented the rest of the code for the user input section, the cart's output section, added the accumulate algorithm to total the cost, and made adjustments to ensure functionality in shopping.cpp.

--- HW6 additional/Shopping/shopping.cpp
+++ HW6 additional/Shopping/shopping.cpp
@@ -19,6 +19,9 @@ using std::setw;
 using std::left;
 using std::right;
 #include <Windows.h>
+#include <numeric>
+#include <vector>
+using std::vector;
 
 struct Record {
 	double unitPrice;
@@ -51,7 +54,10 @@ int main()
 			auto g = item_id[i];
 			item_id[i] = k;
 			auto v = p.second;
-			cout << " " << setw(3) << left << i << setw(15) << left << k << "$" << v.unitPrice << setw(4) << right << v.units << endl;
+			cout << " " << setw(3) << left << i 
+						<< setw(15) << left << k << "$" 
+						<< setw(4) << left << v.unitPrice
+						<< setw(4) << right << v.units << endl;
 			i++;
 		}
 
@@ -62,13 +68,26 @@ int main()
 			cout << "Your cart is empty." << endl;
 		else
 		{
+			i = 1;
 			cout << "Id: " << setw(15) << left << "Item: " << "Price:" << "  Units left: " << endl;
-			for (const auto& p : User_cart)
+			vector <double> item_times_price { 0 };
+			for (const auto& p : item_id)
 			{
-				auto k = p.first;
-				auto v = p.second;
-				cout << " " << setw(3) << left << i << setw(15) << left << k << "$" << v.unitPrice << setw(4) << right << v.units << endl;
+				auto id_ = p.first;
+				auto item_name_ = p.second;
+				auto item_price_ = Shop_items[p.second].unitPrice;
+				auto item_count_ = User_cart[p.second].units;
+				if (item_count_ > 0)
+					cout << " " << setw(3) << left << id_ 
+								<< setw(15) << left << item_name_ << "$" 
+								<< setw(4) << left << item_price_ 
+								<< setw(4) << right << item_count_ << endl;
+				item_times_price.push_back(item_price_ * item_count_);
+				i++;
 			}
+			double total_cost = std::accumulate(item_times_price.begin(), item_times_price.end(), 0.0);
+			cout << endl;
+			cout << "Your total: $" << total_cost << endl;
 		}
 
 		cout << endl;
@@ -85,11 +104,39 @@ int main()
 				break;
 
 			int item_count = item_id.count(id);
-			if (item_count != 0)
+			if (item_count == 0)
 				cout << "There is no item with that id." << endl;
+			else
+				break;
 		}
 		if (id == 0)
 			break;
+		else
+		{
+			cout << endl;
+			cout << "How much of the " << item_id[id] << " do you want to add to or remove from your cart?" << endl;
+			int max_take = Shop_items[item_id[id]].units;
+			int current_item_count = User_cart[item_id[id]].units;
+			cout << "Amount (purchase up to " << max_take << " items or remove up to " << current_item_count << " items from your cart):";
+			int amount = 0;
+			//int total_items = max_take + current_item_count;
+			cin >> amount;
+			if (amount > 0)
+			{
+				if (amount > max_take)
+					amount = max_take;
+				User_cart[item_id[id]].units = amount;
+				Shop_items[item_id[id]].units -= amount;
+			}
+			if (amount < 0)
+			{
+				if (amount < (0 - current_item_count))
+					amount = (0 - current_item_count);
+				User_cart[item_id[id]].units += amount;
+				Shop_items[item_id[id]].units -= amount;
+			}
+		}
+		
 
 
 		system("cls"); // refreshes the console screen.
