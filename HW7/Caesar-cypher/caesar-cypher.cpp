d798c1262def87b5ad6a1b8b8b2df4dcc0d848b6
Nathan Damon <nidamon@alaska.edu>
2020-04-12 19:38:50 -0800

    Created code to break up the user's line into chars and finished the code for getting user input in caesar-cypher.cpp.

--- HW7/Caesar-cypher/caesar-cypher.cpp
+++ HW7/Caesar-cypher/caesar-cypher.cpp
@@ -22,34 +22,69 @@ int main()
 {
 	vector<string> tokens;
 	string user_string;
-	int cypher_shift;
+	string cypher_shift_str;
+	int cypher_shift_val;
 	cout << "Caesar Cypher" << endl;
 	cout << endl;
 	while (true)
 	{
-
+		// Takes users line
 		cout << "Enter a message to cypher (blank line to end): ";
 		std::getline(cin, user_string);
-		if (user_string.size() == 0)
+		if (user_string.size() == 0) // Checks if line is blank
 			break;
 		else
 			StringToTokenWS(user_string, tokens);
+
 		for (size_t i = 0; i < tokens.size(); i++)
 			cout << tokens[i] << endl;
 
-		//while 
+		// Asks for an amount to shift the characters over
 		cout << "Enter an integer to use as the shift: ";
-		cin >> cypher_shift;
+		while (true)
+		{
+			std::getline(cin, cypher_shift_str);
+			istringstream instream(cypher_shift_str);
+			instream >> cypher_shift_val;
+			if (instream) // Confirms if there is an int value in cypher_shift_str
+				break;
+			else
+				cout << "You need to enter an integer: ";
+		}
+		cout << cypher_shift_val << endl;
+
+		// Divides the tokens into chars and outputs them onto the console
+		char c;
+		for (size_t i = 0; i < tokens.size(); i++)
+		{
+			istringstream instream(tokens[i]);
+			for (size_t j = 0; j < tokens[i].size(); j++)
+			{
+				instream >> c;
+				cout << c << " ";
+			}
+			cout << endl;
+		}
+
 	}
 
+
+	int q;
+	cout << endl;
+	cout << "Program has finished. Enter a character to return: ";
+	cin >> q; //Stops the console from closing after the program has run.
 }
 
-unsigned StringToTokenWS(const string& user_string, vector<string>& tokens) // Takes the inputted string and divides it into tokens.
+
+
+ // Takes the inputted string and divides it into tokens.
+unsigned StringToTokenWS(const string& user_string, vector<string>& tokens)
 {
 	istringstream instream(user_string);
 	string s;
 	int n = 0;
-	while (n < user_string.size()) // This is used to add all of the parts of the inputed string or line into the vector tokens.
+	while (n < user_string.size()) // This is used to add all of the parts of 
+								   // the inputed string or line into the vector tokens.
 	{
 		instream >> s;
 		tokens.push_back(s);
