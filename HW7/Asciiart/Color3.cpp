52a3ca7825e76200f71f78351cebc5e0a47a95c1
Nathan Damon <nidamon@alaska.edu>
2020-04-16 14:23:56 -0800

    Created asciiart.cpp.

--- HW7/Asciiart/Color3.cpp
+++ HW7/Asciiart/Color3.cpp
@@ -55,5 +55,6 @@ std::ostream& operator<<(std::ostream& ostr, const Color3& color) {
 
 std::istream& operator>>(std::istream& istr, Color3& color) {
 	// Implement your own input for a Color3
+
 	return istr;
 }
---
+++ HW7/Asciiart/asciiart.cpp
@@ -0,0 +1,16 @@
+/*asciiart.cpp
+Nathan Damon
+CS 201
+4/16/2020
+This program will read a PPM image and then output an ASCII Art version of that image.
+*/
+
+int main()
+{
+
+}
+
+
+
+
+
