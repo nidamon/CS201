a4af6a96738834c9d937248702314ed3e64f9b5b
Nathan Damon <nidamon@alaska.edu>
2020-04-16 13:35:09 -0800

    Created Color3.cpp, Color3.h, Image3.cpp, and Image3.h and began to make additions.

---
+++ HW7/Asciiart/Color3.h
@@ -0,0 +1,32 @@
+// Color3.hpp
+// Interface for Color3 class
+// Original Author: Jonathan Metzgar
+// CS 201 course
+#ifndef COLOR3_H
+#define COLOR3_H
+
+#include <iostream>
+
+class Color3
+{
+public:
+	Color3();
+	Color3(int R, int G, int B);
+
+	// Returns 0.2126R + 0.7152G + 0.0722B
+	int weightedSum() const;
+
+	// Returns an ASCII char representing darkness
+	// e.g. ' ' = WHITE and 'W' = BLACK
+	char asciiValue() const;
+
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+std::ostream& operator<<(std::ostream& ostr, const Color3& color);
+std::istream& operator>>(std::istream& istr, Color3& color);
+
+#endif
+
---
+++ HW7/Asciiart/Image3.h
@@ -0,0 +1,35 @@
+// Image3.hpp
+// Interface for Image3 class
+// Original Author: Jonathan Metzgar
+// CS 201 course
+#ifndef IMAGE3_H
+#define IMAGE3_Cpp
+
+#include <iostream>
+#include <vector>
+#include "Color3.h"
+
+class Image3
+{
+public:
+	Image3(unsigned width, unsigned height);
+
+	const Color3& getPixel(unsigned x, unsigned y) const;
+	void setPixel(unsigned x, unsigned y, const Color3& color);
+
+	bool savePPM(const std::string& path) const;
+	bool loadPPM(const std::string& path);
+
+	void printASCII(std::ostream& ostr) const;
+
+	std::vector<Color3> pixels;
+	unsigned w;
+	unsigned h;
+	Color3 borderColor;
+};
+
+std::ostream& operator<<(std::ostream& ostr, const Image3& image);
+std::istream& operator>>(std::istream& istr, Image3& image);
+
+#endif
+
