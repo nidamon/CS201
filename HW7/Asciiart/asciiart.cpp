775727ffe35b7590a8506bc77859f6bb54ed6e1e
Nathan Damon <nidamon@alaska.edu>
2020-04-16 19:04:27 -0800

    Added in some code to read a file in asciiart.cpp.

--- HW7/Asciiart/asciiart.cpp
+++ HW7/Asciiart/asciiart.cpp
@@ -7,7 +7,38 @@ This program will read a PPM image and then output an ASCII Art version of that 
 
 int main()
 {
+	if (true)
+	{
+		ifstream fin("test.txt");
+		if (!fin)
+		{
+			cout << "Error opening file" << endl;
+			return 0;
+		}
+		else
+		{
+			while (true)
+			{
+				string line;
+				getline(fin, line);
+				if (!fin)
+				{
+					if (fin.eof())
+					{
+						cout << "Finished reading file" << endl;
+					}
+					else
+					{
+						cout << "Error during transmit." << endl;
 
+					}
+					cout << line << endl;
+					break;
+				}
+
+			}
+		}
+	}
 }
 
 
