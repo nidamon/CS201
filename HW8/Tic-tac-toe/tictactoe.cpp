9205cf0efca35302a2715991f08f348a871d8735
Nathan Damon <nidamon@alaska.edu>
2020-04-22 22:29:19 -0800

    Created tictactoe.cpp and tictactoe.h.

---
+++ HW8/Tic-tac-toe/tictactoe.cpp
@@ -0,0 +1,20 @@
+/*tictactoe.cpp
+Nathan Damon
+CS 201
+4/22/2020
+This program will allow the user to play tic-tac-toe on hard difficulty.
+It's predecessor was Grid.cpp from HW1.
+*/
+
+#include "tictactoe.h"
+
+
+int main()
+{
+
+
+	// Stops the console from closing.
+	cout << "Program end" << endl;
+	int q;
+	cin >> q;
+}
---
+++ HW8/Tic-tac-toe/tictactoe.h
@@ -0,0 +1,15 @@
+/*tictactoe.h
+Nathan Damon
+CS 201
+4/22/2020
+This header will declare the needs of tictactoe.cpp.
+*/
+
+#ifndef tictactoe_H
+#define tictactoe_H
+
+
+
+
+
+#endif
